cd51f23c05aca13c4a20214e767d5f1a68ecff91
SkyUnik <fatihjrharum@gmail.com>
2023-04-12 01:10:14 +0700

    Fixing replit config files.

--- replit.nix
+++ replit.nix
@@ -2,7 +2,6 @@
     deps = [
         pkgs.yarn
         pkgs.nodejs
-        pkgs.jellyfin-ffmpeg
         pkgs.nodePackages.npm
         pkgs.nodePackages.typescript
         pkgs.nodePackages.pm2
