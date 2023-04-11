7f726164c8bd38a4f10bf046d727a91b22ea8b8a
SkyUnik <fatihjrharum@gmail.com>
2023-04-12 01:17:41 +0700

    Fixing Chat_GPT API endpoint

--- src/Chat_GPT.ts
+++ src/Chat_GPT.ts
@@ -1,5 +1,7 @@
 import { Configuration, OpenAIApi, ChatCompletionRequestMessage } from "openai";
 import dotenv from "dotenv";
+import axios from "axios";
+
 dotenv.config();
 const mySecret = process.env["OPENAI_API_KEY"];
 export default async function ChatGpt(msg: string) {
@@ -7,22 +9,23 @@ export default async function ChatGpt(msg: string) {
         apiKey: mySecret,
     });
     try {
-        const moderation = await fetch("https://api.openai.com/v1/moderations", {
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${mySecret}`,
-            },
-            method: "POST",
-            body: JSON.stringify({
+        const moderation = await axios.post(
+            "https://api.openai.com/v1/moderations",
+            {
                 input: msg,
-            }),
-        });
-        const moderation_data = await moderation.json();
-        const [result_moderation] = moderation_data.results;
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${mySecret}`,
+                },
+            }
+        );
+        const result_moderation = moderation.data.results[0];
         if (result_moderation.flagged) {
             throw new Error("Query is forbidden by openai");
-            return;
         }
+
         const openai = new OpenAIApi(configuration);
         const messages: ChatCompletionRequestMessage[] = [
             {
